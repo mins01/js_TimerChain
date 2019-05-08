69a878ea5ddfe6184d2e8217f3864bf0fb44a069
공대여자 <mins01.lycos.co.kr@gmail.com>
2019-05-08 15:59:20 +0900

    메소드를 안쪽으러 전부 밀어 넣음.

--- TimerChain.js
+++ TimerChain.js
@@ -2,49 +2,59 @@ var TimerChain = (function(){
 	var TimerChain = function(){	
 	};
 	TimerChain.prototype = Object.create(Array.prototype,{
-		"tm":{ writable: true, configurable: true, value: null },
-		"isStop":{ writable: true, configurable: true, value: false }
-	})
-	var proto = TimerChain.prototype
-	proto.push = function(fn,timeout){
-		this.constructor.prototype.push.call(this,[fn,timeout]);
-		return this;
-	}
-	proto.clear = function(){
-		while(this.length){
-			this.shift();
-		}
-		return this;
-	}
-	proto.start = function(){
-		this.isStop = false;		
-		if(this.tm!=null){ 
-			// console.log("failed .start() : not end setTimeout()");
-			return false;
-		}
-		if(this.length>0){
-				var arr = this.shift();
-				this.tm = setTimeout(function(fn,thisC){
-					return function(){
-						fn();
-						thisC._stop();
-						if(!thisC.isStop){
-							thisC.start();
+		"tm":{ writable: true, value: null },
+		"isStop":{ writable: true, value: false },
+		"push":{
+			value:function(fn,timeout){
+				this.constructor.prototype.push.call(this,[fn,timeout]);
+				return this;
+			}
+		},
+		"clear":{
+			value:function(){
+				while(this.length){
+					this.shift();
+				}
+				return this;
+			}
+		},
+		"start":{
+			value:function(){
+				this.isStop = false;		
+				if(this.tm!=null){ 
+					// console.log("failed .start() : not end setTimeout()");
+					return this;
+				}
+				if(this.length>0){
+					var arr = this.shift();
+					this.tm = setTimeout(function(fn,thisC){
+						return function(){
+							fn();
+							thisC.clearTm();
+							if(!thisC.isStop){
+								thisC.start();
+							}
 						}
-						
-					}
-				}(arr[0],this),arr[1]);
-		}
-		return this;
-	}
-	proto.stop = function(){
-		this.isStop = true;
-		this._stop();
-		return this;
-	}	
-	proto._stop = function(){
-		if(this.tm!=null) clearTimeout(this.tm);
-		this.tm = null;
-	}
+					}(arr[0],this),arr[1]);
+				}
+				return this;
+			}
+		},
+		"stop":{
+			value:function(){
+				this.isStop = true;
+				this._stop();
+				return this;
+			}
+		},
+		"clearTm":{
+			enumerable:false,
+			value:function(){
+				if(this.tm!=null) clearTimeout(this.tm);
+				this.tm = null;
+			}
+		},
+	});
+
 	return TimerChain;
 })()
