f7218416b61327203b9bb773cea373fcc68a4ad4
공대여자 <mins01.lycos.co.kr@gmail.com>
2019-05-17 11:36:49 +0900

    많이 수정.

--- TimerChain.js
+++ TimerChain.js
@@ -10,10 +10,33 @@ var TimerChain = (function(){
 	};
 	TimerChain.prototype = Object.create(Array.prototype,{
 		"tm":{ writable: true, value: null },
-		"isStop":{ writable: true, value: false },
+		"isRunning":{ writable: true, value: false },
+		"lastCallback":{ writable: true, value: null },
+		"lastTimeout":{ writable: true, value: null },
 		"push":{
 			value:function(fn,timeout){
 				this.constructor.prototype.push.call(this,[fn,timeout]);
+				return this;
+			}
+		},
+		"unshift":{
+			value:function(fn,timeout){
+				this.constructor.prototype.unshift.call(this,[fn,timeout]);
+				return this;
+			}
+		},
+		"splice":{
+			value:function(start,deleteCount,fn,timeout){
+				if(arguments.length<2){
+					console.error("arguments.length < 2");
+				}
+				var start = arguments[0];
+				var deleteCount = arguments[1];
+				var args = [start,deleteCount];
+				for(var i=2,m=arguments.length;i<m;i+=2){
+					args.push([arguments[i],arguments[i+1]])
+				}
+				this.constructor.prototype.splice.apply(this,args);
 				return this;
 			}
 		},
@@ -27,29 +50,32 @@ var TimerChain = (function(){
 		},
 		"start":{
 			value:function(){
-				this.isStop = false;		
-				if(this.tm!=null){ 
+				if(this.isRunning){ 
 					// console.log("failed .start() : not end setTimeout()");
 					return this;
 				}
+				this.isRunning = true;
+				return this.run();
+			}
+		},
+		"run":{
+			value:function(){
 				if(this.length>0){
 					var arr = this.shift();
-					this.tm = setTimeout(function(fn,thisC){
+					this.tm = setTimeout(function(cb,thisC){
 						return function(){
-							fn();
-							thisC.clearTm();
-							if(!thisC.isStop){
-								thisC.start();
-							}
+							cb();
+							thisC.run();
 						}
 					}(arr[0],this),arr[1]);
+					this.lastCallback = arr[0];
+					this.lastTimeout = arr[1];
 				}
 				return this;
 			}
 		},
 		"stop":{
 			value:function(){
-				this.isStop = true;
 				this.clearTm();
 				return this;
 			}
